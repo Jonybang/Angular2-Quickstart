96f40aa1c006ef9d2e4fa0fcda3e55c05bb36cb4
Jonybang <jonybang@mail.ru>
2016-06-28 23:41:09 +1000

    multi components and service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,40 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+							<hero-detail [hero]="selectedHero"></hero-detail>
+							<ul>
+								<li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="selectedHero.id === hero.id">
+									{{hero.name}}
+								</li>
+							</ul>`,
+		styles: [`
+			.selected {
+				font-weight: bold;
+			}
+		`],
+		directives: [HeroDetailComponent],
+		providers: [HeroService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+	title = 'lala';
+	heroes: Hero[];
+	selectedHero: Hero;
+
+	constructor(private heroService: HeroService) { }
+
+	ngOnInit() {
+		var self = this;
+		this.heroService.getHeroes().then(function(heroes) {
+			self.heroes = heroes;
+			self.selectedHero = heroes[0];
+		});
+  }
+
+	onSelect(hero: Hero) { this.selectedHero = hero; }
+}
